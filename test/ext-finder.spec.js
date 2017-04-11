877c371e244d58de2dc70ec3782356c0de1be9bd
dlf42 <dolf.the.human@gmail.com>
2017-04-11 23:50:56 +0700

    Return extension

--- test/ext-finder.spec.js
+++ test/ext-finder.spec.js
@@ -1,9 +1,11 @@
 import { expect } from 'chai'
 import { describe, it } from 'mocha'
-import { ext } from '../lib'
+import ext from '../lib'
+
+console.log(ext)
 
 describe('extFinder', () => {
-  it('should start', () => {
+  it('should return an extension', () => {
     let result = ext('ext-finder.spec.js')
     expect(result).to.equal('js')
   })
