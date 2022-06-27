ff3d340537174e82c3adb35eb544df29f4b9e15c
Dolf Barr <mail@dolf.me>
2022-06-27 14:07:52 +0200

    Upgrade eslint

--- test/ExtFinder.spec.js
+++ test/ExtFinder.spec.js
@@ -4,19 +4,19 @@ import extFinder from '../lib'
 
 describe('ExtFinder', () => {
   it('should return an extension...', () => {
-    let result = extFinder.getExtension('ExtFinder.spec.js')
+    const result = extFinder.getExtension('ExtFinder.spec.js')
     expect(result).to.equal('js')
   })
   it('should return an empty extension...', () => {
-    let result = extFinder.getExtension('testFile')
+    const result = extFinder.getExtension('testFile')
     expect(result).to.equal(undefined)
   })
   it('should return an extension for file with spaces...', () => {
-    let result = extFinder.getExtension('test file.js')
+    const result = extFinder.getExtension('test file.js')
     expect(result).to.equal('js')
   })
   it('should return an array of extensions for array of files...', () => {
-    let result = extFinder.getExtension([
+    const result = extFinder.getExtension([
       'text.c',
       'text.py',
       'test.js'
